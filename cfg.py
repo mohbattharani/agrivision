8d3ff0325e9389ce8de4942f72654daf8d5e8f8b
abdulwahabamin <abdulwahabamin@yahoo.com>
2020-03-17 22:09:54 +0500

    three cam test

--- cfg.py
+++ cfg.py
@@ -2,4 +2,6 @@ cam_ids = ['lums']
 mongo_cfg = {'db_server': {'host': 'mongo', 'port': '27017'}, 'db_name': 'trash', 'db_raw_clc': 'main', 'db_manual_clc': 'manual'}
 directories = {'main_dir': 'camfeed', 'save_dir': 'results'}
 # camid: {longitude,latitude,description}
-cam_info = {'lums': {'longitude': 31.472780, 'latitude': 74.409863, 'description': 'Roohi Nala'}}
+cam_info = {'lums': {'longitude': 31.472780, 'latitude': 74.409863, 'description': 'Roohi Nala'},
+            'test1': {'longitude': 31.472780, 'latitude': 74.409863, 'description': 'Roohi Nala 1'},
+            'test2': {'longitude': 31.472780, 'latitude': 74.409863, 'description': 'Roohi Nala 2'}}
