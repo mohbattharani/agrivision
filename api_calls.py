560bd8c267f8f817d9a2bad82dd6821dc7e6d4c7
abdulwahabamin <abdulwahabamin@yahoo.com>
2020-04-03 04:03:40 +0500

    graph api calls

--- api_calls.py
+++ api_calls.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient, GEO2D
 # noinspection PyUnresolvedReferences
 import cfg
-
+from datetime import datetime, timedelta
+import numpy as np
 # Mongo initialization
 client = MongoClient(cfg.mongo_cfg.get('db_server').get('host'), int(cfg.mongo_cfg.get('db_server').get('port')))
 
@@ -13,14 +14,17 @@ class api_call:
         # specify which collection to use
         self.collection = self.db[clc]
 
-    def image_count(self):
-        return self.collection.count()
+    def image_count(self, camid=None):
+        if camid is None:
+            return self.collection.count()
+        else:
+            return self.collection.find({"cam_id": camid})
 
-    def image_count_camid(self, camid):
-        return self.collection.find({"cam_id": camid})
-
-    def trash_count(self):
-        documents = self.collection.find({"Predictions": {"$exists": True}})
+    def trash_count(self, camid=None):
+        if camid is None:
+            documents = self.collection.find({"Predictions": {"$exists": True}})
+        else:
+            documents = self.collection.find({"cam_id": camid, "Predictions": {"$exists": True}})
         count = 0
         for document in documents:
             pred = document.get("Predictions")
@@ -28,14 +32,36 @@ class api_call:
 
         return count
 
-    def trash_count_camid(self, camid):
-        documents = self.collection.find({"cam_id": camid, "Predictions": {"$exists": True}})
-        count = 0
+    def graph_day(self, date, camid=None):
+        if camid is None:
+            documents = self.collection.find({"date": date, "Predictions": {"$exists": True}})
+        else:
+            documents = self.collection.find({"cam_id": camid, "date": date, "Predictions": {"$exists": True}})
+        times = []
+        count = []
         for document in documents:
             pred = document.get("Predictions")
-            count += len(pred)
+            times.append(document.get("time"))
+            count.append(len(pred))
 
-        return count
+        return times, count
+
+    def graph_range(self, start_date, end_date, date_format='%Y-%m-%d', camid=None):
+        # start and end date must be in the format YYYY-MM-DD
+        days_diff = (datetime.strptime(start_date, date_format) - datetime.strptime(end_date, date_format)).days
+        dates = np.array([(start_date + timedelta(inc)).strftime('%Y-%m-%d') for inc in range(0, days_diff + 1)])
+        count = np.zeros_like(dates)
+        if camid is None:
+            documents = self.collection.find({"date": {'$in': dates}, "Predictions": {"$exists": True}})
+        else:
+            documents = self.collection.find({"cam_id": camid, "date": {'$in': dates}, "Predictions": {"$exists": True}})
+        for document in documents:
+            pred = document.get("Predictions")
+            date = document.get("date")
+            index = np.where(dates==date)[0]
+            count[index] += len(pred)
+
+        return dates, count
 
 
 if __name__ == '__main__':
