0625b63b85d6d6d6ea9d29ae11f433f1c3940674
abdulwahabamin <abdulwahabamin@yahoo.com>
2020-04-17 04:24:47 +0500

    dummy data generation

--- cfg.py
+++ cfg.py
@@ -1,8 +1,9 @@
 mongo_cfg = {'db_server': {'host': 'mongo', 'port': '27017'}, 'db_name': 'trash', 'db_raw_clc': 'main', 'db_manual_clc': 'manual'}
 directories = {'main_dir': 'camfeed', 'save_dir': 'results'}
 # camid: {longitude,latitude,description}
-cam_info = {'lums_1': {'longitude': 31.472780, 'latitude': 74.409863, 'description': 'Roohi Nala'},
-            'lums_2': {'longitude': 31.478238, 'latitude': 74.413239, 'description': 'Roohi Nala LUMS backside'},
+# Don't use underscore in camera id
+cam_info = {'lums1': {'longitude': 31.472780, 'latitude': 74.409863, 'description': 'Roohi Nala'},
+            'lums2': {'longitude': 31.478238, 'latitude': 74.413239, 'description': 'Roohi Nala LUMS backside'},
             'comsats': {'longitude': 31.398038, 'latitude': 74.211902, 'description': 'Hudiara Drain Comsats'}}
             # 'test1': {'longitude': 31.472780, 'latitude': 74.409863, 'description': 'Roohi Nala 1'},
             # 'test2': {'longitude': 31.472780, 'latitude': 74.409863, 'description': 'Roohi Nala 2'}}
