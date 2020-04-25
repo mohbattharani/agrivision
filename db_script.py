1cb4b05af80806f1c152f74cd01d140e14beaf32
abdulwahabamin <abdulwahabamin@yahoo.com>
2020-04-25 05:39:57 +0500

    db data fix

--- db_script.py
+++ db_script.py
@@ -85,4 +85,4 @@ class DbUp:
 if __name__ == '__main__':
     server = DbUp(cfg.mongo_cfg.get('db_name'), cfg.mongo_cfg.get('db_raw_clc'))
     # server.update_24()
-    server.get_all_data('2020-04-17')
+    server.get_all_data('2020-04-25')
