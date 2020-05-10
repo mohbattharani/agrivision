b060d1a1bf438f03a33c0cabb68d510b7870f508
abdulwahabamin <abdulwahabamin@yahoo.com>
2020-05-10 06:35:40 +0500

    studio

--- sample_graphs.py
+++ sample_graphs.py
@@ -22,7 +22,8 @@ def get_data(url, start_date, end_date, cam_id=None, date_format='%Y-%m-%d'):
         req = json.dumps(req)
         x = requests.post(url, data=req)
         x = json.loads(x.text)
-        if x['status'] is False:
+        status = x.get('status')
+        if status is False:
             continue
         times, trash_count = zip(*x.items())
         trash_count = list(map(int, trash_count))
