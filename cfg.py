3c13e4c76921dfae6254a44c994f0f15f4f841d3
Ted <abdulwahabamin@yahoo.com>
2020-06-15 12:10:47 +0500

    flask app and gsheets integration added

--- cfg.py
+++ cfg.py
@@ -7,6 +7,6 @@ cam_info = {'lums1': {'longitude': 74.409863, 'latitude': 31.472780, 'descriptio
             'comsats': {'longitude': 74.211902, 'latitude': 31.398038, 'description': 'Hudiara Drain Comsats'}}
             # 'test1': {'longitude': 31.472780, 'latitude': 74.409863, 'description': 'Roohi Nala 1'},
             # 'test2': {'longitude': 31.472780, 'latitude': 74.409863, 'description': 'Roohi Nala 2'}}
-api_urls = {'range_data': 'http://0.0.0.0:5000/range_graph',
-            'day_time_data': 'http://0.0.0.0:5000/day_graph',
-            'total_trash': 'http://0.0.0.0:5000/total_trash'}
+api_urls = {'range_data': 'http://flask_app:5000/range_graph',
+            'day_time_data': 'http://flask_app:5000/day_graph',
+            'total_trash': 'http://flask_app:5000/total_trash'}
